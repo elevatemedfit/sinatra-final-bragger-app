8841114253a09ba65dc22ebffd434bf603aa66c7
Chris Junker <elevatemedfit@gmail.com>
2019-10-07 10:28:31 -0400

    added helper_methods for logged_in? and current_user.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,10 +6,20 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    use Rack::Flash, :sweep => true
   end
 
   get "/" do
-    erb :welcome
+    erb :index
   end
 
+  helper_methods do
+    def logged_in?
+      !!session[:user_id]
+    end
+
+    def current_user
+      User.find(session[:user_id])
+    end
+  end
 end
