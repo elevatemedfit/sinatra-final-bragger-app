de5a1dbd811b11961bbf4f067a5024aa29506222
Chris Junker <elevatemedfit@gmail.com>
2019-10-08 15:08:19 -0400

    Added controller actions for all posts and new posts in post class.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,4 +3,21 @@ require 'rack-flash'
 
 
 class PostsController < ApplicationController
+
+    get '/posts' do 
+        if logged_in?
+            @posts = Post.all
+            erb :'/posts/index'
+        else
+            redirect '/login'
+        end
+    end
+
+    get '/posts/new' do
+        if logged_in?
+            erb :'/posts/create'
+        else 
+            redirect '/login'
+        end
+    end
 end
