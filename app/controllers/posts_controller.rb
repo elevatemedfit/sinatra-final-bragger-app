aa07ad420eb521256fb0d0f26a9b64468d12341c
Chris Junker <elevatemedfit@gmail.com>
2019-10-08 15:23:17 -0400

    Set up the post controller action for new post creation based on valid users.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,4 +20,24 @@ class PostsController < ApplicationController
             redirect '/login'
         end
     end
+
+    post '/posts' do
+        if params.empty?
+            flash[:error] = "All fields must be filled in"
+            redirect '/posts/new'
+        elsif logged_in? && !params.empty?
+            @post = current_user.posts.create(name: params[:name], user_id: params[:user_id], content: params[:content], image_url: params[:image_url], step_count: params[:step_count], cardio_score: params[:cardio_score], caloric_burn: params[:caloric_burn], walking_distance: params[:walking_distance])
+            if @post.save
+                redirect "/posts/#{@post.id}"
+            else
+                flash[:error] = "Your post could not be saved. Try again!"
+                redirect '/posts/new'
+            end
+        else 
+            flash[:error] = "You must be logged in to see the post index."
+            redirect '/login'
+        end
+        current_user.save
+    end
+
 end
