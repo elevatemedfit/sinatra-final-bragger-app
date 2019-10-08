46e99950627d0caacfb932e136082c8af52e3b75
Chris Junker <elevatemedfit@gmail.com>
2019-10-08 15:05:42 -0400

    Added validate statements to users and posts models.

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,9 @@ class Post < ActiveRecord::Base
 
     extend Slugify::ClassMethods
     include Slugify::InstanceMethods
-    
+
     belongs_to :user
+
+    validates_presence_of :name, :content, :image_url, :step_count, :cardio_score, :caloric_burn, :walking_distance
     
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,12 @@
 class User < ActiveRecord::Base
-    
+
     extend Slugify::ClassMethods
     include Slugify::InstanceMethods
 
 has_secure_password #allows us to use bcrypt.
+
+validates_presence_of :username, :email, :password 
+validates :username, :uniqueness => {:case_sensitive => false}
 
 has_many :posts
 end
